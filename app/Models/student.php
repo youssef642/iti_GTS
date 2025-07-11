5e9df369f0d6309bece8d5c7523477e433a9939a
ahmedmohmed213 <ahmedmohamed2000321@gmail.com>
2025-07-11 20:14:11 +0300

    Update student model

--- app/Models/student.php
+++ app/Models/student.php
@@ -11,25 +11,11 @@ use Laravel\Sanctum\HasApiTokens;
 class Student extends Authenticatable
 {
     use HasApiTokens, Notifiable;
-//con
+
     protected $fillable = [
         'name', 'email', 'password', 'phone', 'age', 'gender',
         'faculty', 'university', 'track', 'image', 'CV'
     ];
 
     protected $hidden = ['password'];
-
-    public function skills()
-    {
-        return $this->hasMany(Skill::class);
-    }
-    public function jobApplications()
-    {
-        return $this->hasMany(JobApplication::class, 'student_id');
-    }
-    public function company()
-    {
-        return $this->belongsTo(Company::class, 'company_id');
-    }
-
 }
