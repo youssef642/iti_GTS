f8be4007396ef6d6969a0aeecbe80648bb157c44
youssef642 <youssefaboseif72@gmail.com>
2025-07-15 02:39:21 +0300

    edits

--- app/Models/student.php
+++ app/Models/student.php
@@ -1,6 +1,6 @@
 <?php
 
-//  app/Models/Student.php
+// app/Models/Student.php
 
 namespace App\Models;
 
@@ -14,27 +14,8 @@ class Student extends Authenticatable
 
     protected $fillable = [
         'name', 'email', 'password', 'phone', 'age', 'gender',
-        'faculty', 'university', 'track', 'image', 'duration_track', 'address' , 'company_id'
-
+        'faculty', 'university', 'track', 'image', 'CV'
     ];
 
     protected $hidden = ['password'];
-
-    public function skills()
-    {
-        return $this->hasMany(Skill::class);
-    }
-    public function jobApplications()
-    {
-        return $this->hasMany(JobApplication::class, 'student_id');
-    }
-    public function company()
-    {
-        return $this->belongsTo(Company::class, 'company_id');
-    }
-    public function experience()
-    {
-        return $this->hasMany(Experience::class, 'student_id');
-    }
-
 }
